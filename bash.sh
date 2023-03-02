f77017e93e908d609a5d1c452e4ca87763308293
bosilu09 <84651759+bosilu09@users.noreply.github.com>
2023-03-02 14:06:34 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,13 +1,1 @@
-apt update
-apt upgrade
-pkg update && pkg upgrade
-pkg install bash
-pkg install libwebp
-pkg install git -y
-pkg install nodejs -y 
-pkg install ffmpeg -y 
-pkg install wget
-pkg install imagemagick -y
-git clone https://github.com/DGXeon/CheemsBot-MD4
-cd CheemsBot-MD
-npm start
+apt update && apt upgrade && pkg update && pkg upgrade && pkg install bash && pkg install libwebp && pkg install git -y && pkg install nodejs -y  && pkg install ffmpeg -y  && pkg install wget && pkg install imagemagick -y && git clone https://github.com/DGXeon/CheemsBot-MD4 && cd CheemsBot-MD && npm start
